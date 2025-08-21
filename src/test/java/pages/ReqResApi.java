05add0d590fc467be0d3d57c30adc5aab52ccae1
Nanda Nugraha <nandanugg@gmail.com>
2025-08-21 15:16:45 +0700

    add api

---
+++ src/test/java/pages/ReqResApi.java
@@ -0,0 +1,40 @@
+package pages;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class ReqresApiPage {
+    private String baseUrl = "https://reqres.in/";
+    private Response response;
+
+    public void setEndpoint(String endpoint) {
+        RestAssured.baseURI = baseUrl + endpoint;
+    }
+
+    public Response sendGetRequest() {
+        response = given()
+                .when()
+                .get()
+                .then()
+                .extract()
+                .response();
+        return response;
+    }
+
+    public Response sendPostRequest(String requestBody) {
+        response = given()
+                .header("Content-Type", "application/json")
+                .header("x-api-key", "reqres-free-v1")
+                .body(requestBody)
+                .when()
+                .post()
+                .then()
+                .extract()
+                .response();
+        return response;
+
+    }
+
+}
